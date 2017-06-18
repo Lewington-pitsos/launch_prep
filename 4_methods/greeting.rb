c3f31758a0f53b87bd2fb91258f68ae5048da46c
Lewington <lewington@student.unimelb.edu.au>
2017-06-18 22:28:15 +1000

    submitting the 4th chapter(methods)  excercises

---
+++ 4_methods/greeting.rb
@@ -0,0 +1,22 @@
+#excercise 1
+
+def greet name
+  x = "hello #{name}, please, go fuck yourself"
+  p x
+end
+
+puts (greet "fred" )
+
+#excercise 3
+def times a, b
+  result = a * b
+end
+
+#excercise 5
+def scream(words)
+  words = words + "!!!!"
+  puts words
+  words
+end
+
+scream("Yippeee")
